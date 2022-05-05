0ca4ca8d19516a0105d1cd6c8e187623edcc9c97
Toshika Raghuwanshi <raghuwanshi.toshika17@gmail.com>
2022-05-05 13:13:25 +1000

    js comment

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-
+//ADD SECTIONS BY USING FOR LOOP//
 const sections = document.getElementsByTagName("section")
 const length = sections.length;
 for (let i = 0; i < length; i++) {
     document.getElementsByClassName('navbar')[0].innerHTML += `<li id="section-${i+1}menu" class="active">Section ${i+1}</li>`;
 
 }
-
+//SCROLL EVENT USED ONE BY ONE START//
 document.getElementById("section-1menu").addEventListener('click', () => {
     document.getElementById('section-1').scrollIntoView({
         behavior: 'smooth'
@@ -29,6 +29,45 @@ document.getElementById("section-4menu").addEventListener('click', () => {
     });
 });
 
+//SCROLL EVENT USED ONE BY ONE START//
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fetch("https://get.geojs.io/v1/ip/country.json?ip=8.8.8.8")
+  .then(function(data) {
+    return data.json();
+  })
+  .then(function(response) {
+    console.log(JSON.stringify(response));
+  })
 
 
